05c5dfc5cdc70715cdaa5a36407d1e5fda7b0db8
Ashley Blake-Hood <ashbh@hotmail.co.uk>
2022-08-20 17:38:13 +0100

    feat: add ssl to db constring

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 
 const { Pool } = require('pg');
 
-const pool = new Pool({connectionString:process.env.DATABASE_URL,});
+const pool = new Pool({connectionString:`${process.env.DATABASE_URL}?sslmode=require`,});
 
 module.exports = {
     async query(text, params) {
