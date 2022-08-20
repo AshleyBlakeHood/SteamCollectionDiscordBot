7e68750d6a1c4a41824aaccc9b0126c4500caa26
Ashley Blake-Hood <ashbh@hotmail.co.uk>
2022-08-20 17:42:07 +0100

    feat: turn off CA protection for db

--- db/index.js
+++ db/index.js
@@ -1,7 +1,12 @@
 
 const { Pool } = require('pg');
 
-const pool = new Pool({connectionString:`${process.env.DATABASE_URL}?sslmode=require`,});
+const pool = new Pool({
+  connectionString:`${process.env.DATABASE_URL}?sslmode=require`, 
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 module.exports = {
     async query(text, params) {
